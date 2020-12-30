3f3731c25887732899fbf0227cd283b507720738
Cleo Credo <credocleo28@gmail.com>
2020-12-30 20:41:22 +0800

    initial files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div style={{color: `purple`, fontSize: `72px`}}>
+      <Link to="/contact/">Contact us</Link>
+      <h1>Hello PH!</h1>
+      <p>What a world!</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
 }
